ae8963c291d96d3e5dd5abacd2349715dd184688
LeandroDevLab <prof.oliota@gmail.com>
2025-08-24 19:03:30 -0300

    Estilização inicial

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Comando para criar o projeto
 
 `npm create vite@latest meu-app-vite -- --template react`
 
+npm i react-icons
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,16 @@
 //Usando Component para seguir exemplo do professor
 import React, { Component } from "react";
 
+//Form
+import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 import "./Main.css";
 
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Fazer", "Beber", "Estudar"],
   };
 
   handleChange = (e) => {
@@ -15,16 +20,35 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
-        <h1>{novaTarefa}</h1>
+        <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" name="novaTarefa" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            className="input_novaTarefa"
+            type="text"
+            value={novaTarefa}
+            name="novaTarefa"
+          />
+          <button type="submit">+{/*  <FaPlus /> */}</button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            // atribuindo um key único, o próprio index
+            <li key={index}>
+              {tarefa}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
